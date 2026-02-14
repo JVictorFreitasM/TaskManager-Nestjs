fe0e4966a3a7d2323e2b4b9b454a0fdd933f1fbe
User <joaovictorddm@gmail.com>
2026-02-13 21:47:54 -0300

    Adicionado a validação de dados das Tasks

--- src/task/task.dto.ts
+++ src/task/task.dto.ts
@@ -1,8 +1,50 @@
+import {
+  IsDateString,
+  IsEnum,
+  IsOptional,
+  IsString,
+  IsUUID,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export enum TaskStatusEnum {
+  TO_DO = 'TO_DO',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
+
 export class TaskDto {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsUUID()
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsOptional()
   id: string;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsString()
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @MinLength(3)
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @MaxLength(256)
   title: string;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsString()
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @MinLength(5)
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @MaxLength(512)
   description: string;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsEnum(TaskStatusEnum)
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsOptional()
   status: string;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsDateString()
   expirationDate: Date;
 }
 
